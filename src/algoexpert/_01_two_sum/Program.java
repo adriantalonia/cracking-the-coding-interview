222fd4d4193d4337881d21e4c0178fcb63c5c5b2
Adrian <adrian.talonia11@gmail.com>
2022-08-05 23:23:43 -0500

    algoexpert problem

---
+++ src/algoexpert/_01_two_sum/Program.java
@@ -0,0 +1,43 @@
+package algoexpert._01_two_sum;
+
+import java.util.*;
+
+class Program {
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        // Write your code here.
+        int x = 0;
+        int y = 0;
+        boolean change = false;
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = i+1; j<  array.length; j++){
+                if(array[i]+array[j] == targetSum){
+                    x = array[i];
+                    y = array[j];
+                    change = true;
+                    break;
+                }
+            }
+            if(change) break;
+        }
+
+        return change ? new int[]{x,y} : new int[]{};
+    }
+}
+
+class ProgramTest {
+    //@Test
+    public static void main(String[] args){
+        int[] output = Program.twoNumberSum(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10);
+        if(output.length == 2) System.out.println("2");;
+        if(contains(output, -1)) System.out.println("-1");;
+        if(contains(output, 11)) System.out.println("11");;
+    }
+
+    public static boolean contains(int[] output, int val) {
+        for (var el : output) {
+            if (el == val) return true;
+        }
+        return false;
+    }
+}
