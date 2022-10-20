318a86a18f87e930390cb42fa77c5438e2276570
Adrian <adrian.talonia11@gmail.com>
2022-10-19 23:30:58 -0500

    add a new problem

---
+++ src/cracking_the_coding_interview_book/_02_data_structures/interview_questions/_1_3_ur_lify/URLify.java
@@ -0,0 +1,41 @@
+package cracking_the_coding_interview_book._02_data_structures.interview_questions._1_3_ur_lify;
+
+public class URLify {
+
+    public static void main(String[] args) {
+        String test = "Mr John Smith    ";
+        char[] test2 = test.toCharArray();
+        replaceSpaces(test2, 13);
+        System.out.println(test2);
+    }
+
+    public static void replaceSpaces(char[] str, int trueLength) {
+        int numberOfSpaces = countOfChar(str, 0, trueLength, ' ');
+        int newIndex = trueLength - 1 + numberOfSpaces * 2;
+
+        if (newIndex + 1 < str.length) str[newIndex + 1] = '\0';
+        for (int oldIndex = trueLength - 1; oldIndex >= 0; oldIndex -= 1) {
+            if (str[oldIndex] == ' ') {
+                str[newIndex] = '0';
+                str[newIndex - 1] = '2';
+                str[newIndex - 2] = '%';
+                newIndex -= 3;
+            } else {
+                str[newIndex] = str[oldIndex];
+                newIndex -= 1;
+            }
+        }
+    }
+
+    public static int countOfChar(char[] str, int start, int end, int target) {
+        int count = 0;
+        for (int i = start; i < end; i++) {
+            if (str[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
